571262b5a733a4f8a6c277893cc3ea530626c6a2
regiostech <ultralammed@yahoo.com>
2015-05-12 22:48:16 -0400

    Yes!

---
+++ DirToIso/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using DiscUtils.Iso9660;
+
+namespace DirToIso
+{
+    class Program
+    {
+        static Dictionary<string, string> options = new Dictionary<string, string>
+        {
+            {"d", "Specifies input directory path"},
+            {"o", "Specifies output filename"},
+            {"id", "Specifies an output volume identifier"},
+            {"h", "Shows this help view."}
+        };
+
+        private static void Main(string[] args)
+        {
+            var inputDir = Directory.GetCurrentDirectory();
+            var outputFn = Path.Combine(inputDir, Path.ChangeExtension(Path.GetFileName(inputDir), "iso"));
+            var identifier = Path.GetFileName(inputDir);
+
+            var lastArg = string.Empty;
+            foreach (var arg in args)
+            {
+                if (arg.TrimStart('-').Trim().ToLower() == "h")
+                {
+                    ShowHelp();
+                    return;
+                }
+                switch (lastArg)
+                {
+                    case "d":
+                        inputDir = arg;
+                        break;
+                    case "o":
+                        outputFn = arg;
+                        break;
+                    case "id":
+                        identifier = arg;
+                        break;
+                }
+                lastArg = arg.TrimStart('-').Trim().ToLower();
+            }
+            if (identifier.Length > 32)
+                identifier = identifier.Substring(0, 32);
+            var builder = new CDBuilder
+            {
+                UseJoliet = true,
+                VolumeIdentifier = Regex.Replace(identifier, "[^\\w|_]", "").Trim().ToUpper()
+            };
+            MakeIso(builder, inputDir, inputDir);
+            builder.Build(outputFn);
+        }
+
+        static void ShowHelp()
+        {
+            Console.WriteLine("Syntax: dirtoiso [options]");
+            Console.WriteLine("Options:");
+            foreach (var option in options)
+            {
+                Console.WriteLine("\t-{0}: {1}.", option.Key, option.Value);
+            }
+        }
+
+        static void MakeIso(CDBuilder builder, string topdir, string dir, bool inner = false)
+        {
+            foreach (var directory in Directory.GetDirectories(dir))
+            {
+                MakeIso(builder, topdir, directory, true);
+            }
+            foreach (var file in Directory.GetFiles(dir))
+            {
+                var fn = file.Replace(topdir, "");
+                builder.AddFile(fn, file);
+            }
+        }
+    }
+}
---
+++ DirToIso/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DirToIso")]
+[assembly: AssemblyDescription("Generates an ISO file from a given directory on a Windows system.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Regios Technologies")]
+[assembly: AssemblyProduct("DirToIso")]
+[assembly: AssemblyCopyright("Copyright © T. Osakwe 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a3d0629f-0467-4664-8421-3179a5bb5a48")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
